299a592885ece6b836d7dbaa1c4011f9029b3399
Osmond <105639028+osmondcode@users.noreply.github.com>
2022-09-01 04:05:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const list = document.querySelectorAll('.list');
+function activeLink(){
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click',activeLink))
+
+
+function osmond(){
+    var o = document.querySelector(".dropdown-link");
+    if(o.){
+
+    }
+}
